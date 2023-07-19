f2b41fd38f3bd69fea81c760461bd42517410e97
eagf <eagf@odoo.com>
2023-07-19 09:38:35 -0600

    Primer modulo

---
+++ custom_modules/motorcycle-registry/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': "Motorcycle Registry",
+    'summary':"Manage Registration of Motorcycles",
+    'description': """Motorcycle Registry
+    ============================
+    This Module is used to keep track of the Motorcycle Registration and Ownership of each motorcycle of tehe brand""",
+    'author': "eagf",
+    'category': "Kawiil",
+    'website': "https://www.github.com/eagf-odoo",
+    'application': True
+}
